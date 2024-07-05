14af1df2421ade10a4a54994bb0606fde178d01b
aquarius-wing <aquarius_wing@icloud.com>
2024-07-05 10:49:55 +0800

    feat(AIChat): update Primitive of AIChat

--- packages/radix-vue/src/AIChat/AIChatContent.vue
+++ packages/radix-vue/src/AIChat/AIChatContent.vue
@@ -1,11 +1,26 @@
+<script lang="ts">
+import type { PrimitiveProps } from '@/Primitive'
+
+export interface AIChatContentProps extends PrimitiveProps {}
+</script>
+
 <script setup lang="ts">
 import { CollectionSlot } from '@/Collection'
 import { Primitive } from '@/Primitive'
+
+const props = withDefaults(defineProps<AIChatContentProps>(), {
+  as: 'div',
+  prompt: '',
+  messages: () => [],
+})
 </script>
 
 <template>
   <CollectionSlot>
-    <Primitive as="div">
+    <Primitive
+      :as="as"
+      :as-child="asChild"
+    >
       <slot />
     </Primitive>
   </CollectionSlot>
